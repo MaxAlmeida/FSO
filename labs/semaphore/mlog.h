b08f010973ce4650c025522e5d7bae76fe0f1589
MaxAlmeida <llewxam150@gmail.com>
2016-05-01 11:01:41 -0300

    Adding log lib

---
+++ labs/semaphore/mlog.h
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+bool EN_LOG=false;
+
+void dlog(char* msg){
+  if(EN_LOG)
+    printf("DEBUG: %s\n",msg);
+}
+
+void dilog(char* msg, int arg){
+  if(EN_LOG)
+    printf("DEBUG: %s : %d\n",msg,arg);
+}
+
+void dmlog(char* msg, char* arg){
+  if(EN_LOG)
+    printf("DEBUG: %s : %s\n",msg,arg);
+}
+
+void elog(char* msg){
+  printf("ERROR: %s\n",msg);
+}
+
+void mlog(char* msg){
+  if(EN_LOG)
+    printf("MSG: %s\n",msg);
+}
+
