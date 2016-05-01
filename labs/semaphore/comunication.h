361a6759a4bf500cf509fd3fea021a378b6e4fce
MaxAlmeida <llewxam150@gmail.com>
2016-04-30 23:53:13 -0300

    Initiates minimal structure for development of the program
    Signed-off-by: MaxAlmeida <llewxam150@gmail.com>
    Signed-off-by: Paulo Tada <paulohtfs@gmail.com>

--- labs/semaphore/comunication.h
+++ labs/semaphore/comunication.h
@@ -1,8 +1,9 @@
+#include <errno.h>
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-
-extern int errno;
+#include <semaphore.h>
+#include "mlog.h"
 /* Definition of the Shared Memory attr */
 #define SHM_PRM 0600
 #define SHM_LEN  250
@@ -17,8 +18,9 @@ char* user_input(){
 /********************************
     Shared Memory functions
 ********************************/
-int shm_create(key_t key){
-  int shmid = shmget(key, SHM_LEN, IPC_CREAT | 0666);
+int shm_create(Sems* semaphore){
+  key_t key = 5678;
+  int shmid = shmget(key, sizeof(semaphore) , IPC_CREAT | 0666);
   if(shmid < 0){
     elog("Failed to create share memory!");
     mlog(strerror(errno));
@@ -28,7 +30,8 @@ int shm_create(key_t key){
   return shmid;
 }
 
-int shm_get(key_t key){
+int shm_get(){
+  key_t key = 5678;
   int shmid = shmget(key, SHM_LEN, 0666);
   if(shmid < 0){
     mlog("No message on shared memory!");
@@ -36,31 +39,32 @@ int shm_get(key_t key){
   return shmid; 
 }
 
-char* shm_attach(int shmid){
-  char* shm = shmat(shmid, NULL, 0);
-  if(shm == (char *) -1){
+Sems* shm_attach(int shm_id){
+  key_t key = 5678;
+  Sems* shm = (Sems*) shmat(shm_id, NULL, 0);
+  if(shm == (Sems *) -1){
     elog("Failed to attach memory!");
     mlog(strerror(errno));
   }
   return shm;
 }
 
-void shm_send(char msg,char* shm){
+void shm_send(Sems* msg,sem_t* shm){
   strcpy(shm, msg);
 }
 
-void shm_write_process(char msg_send){
+void shm_write_process(Sems* msg_send){
   key_t key = 5678;
   int shmid = shm_get(key);
   char* shm = shm_attach(shmid);
   shm_send(msg_send,shm);
 }
 
-char* shm_read_process(){
+Sems* shm_read_process(int shm_id){
   key_t key = 5678;
   int shmid = shm_get(key);
-  char* shm = shm_attach(shmid); 
- return shm;
+  Sems* shm = shm_attach(shm_id); 
+  return shm;
 }
 
 void clear_memmory(char* shm, int shmid){
--- labs/semaphore/main.c
+++ labs/semaphore/main.c
@@ -21,131 +21,81 @@
 #include <stdlib.h>
 #include <time.h>
 #include <semaphore.h>
-
-#define SENADOR   0
-#define DEPUTADO  1
-#define VEREADOR  2
-int N; //numero de parlamentares
-int state[N];
-Parlamentar parlamentares[N];
-sem_t mutex;
-int status[3];
-void parlamentar(int i){
-  srand(time(NULL));
-  usleep(rand() % 1000);
-  enter_room();
-  vote();
-  quit_room();
-}
-/*
-void take_fork(int i){
-  down(&mutex);  // area crítica
-  state[i] == HUNGRY; // READY
-  test(i);
-  up(&mutex);  // sai da area critica
-  down(&s[i]);  // block entrada da sala
-}
-
-void put_fork(int i){
-  down(&mutex);
-  state[i] = THINKING;
-  test(LEFT);
-  test(RIGHT);
-  up(&mutex);
-}
-
-void test(int i){ // numero do filosofo
-  if(status[i]==HUNGER && status[LEFT]!= EATING && status[RIGHT]!= EATING){
-    status[i] = EATING;// VOTANDO
-    up(&s[i]);
-  }
-}
-*/
-
-void meditate(){
-  srand(time(NULL));
-  int time = rand() % 1000;
-  usleep(time);
-}
-
-void enter_room(int i){
-  sem_wait(&mutex);
-  state[i] = READY
-  test(i);
-  sem_post(&mutex);
-  sem_wait(&parlamentares[i]);
-}
-
-void quit_room(int i){
-  sem_wait(&mutex);
-  state[i] = DONE;
-  test(i);
-
-}
-
-void vote(){
-  usleep(50);
-}
-
+#define SENADOR   1
+#define DEPUTADO  2
+#define VEREADOR  3
 typedef struct sems{
   sem_t senador;
-  sem_t vereador;
   sem_t deputado;
+  int senador_only;
 }Sems;
 
-typedef struct parlamentar{
-  int type;
-}Parlamentar;
+#include "comunication.h"
+void meditate(int number){
+  printf("Eu processo de numero %d pensando aqui na morte da bezerra\n",number);
+
+  srand(time(NULL));
+  int time = rand() % 1000;
+  sleep(1);
+}
+void enter_room(int number){
+  printf("Processo de numero %d entra na sala\n",number);
+}
+
+void vote(int number){
+  printf("Processo de numero %d vota neste momento\n",number);
+  sleep(1);
+}
+
+void voting_process(int number, int shmid){ 
+  Sems* semaphore = shm_read_process(shmid);
+  meditate(number);
+  sem_wait(&semaphore->senador); // Enter Critical Section
+  semaphore->senador_only = 1; //only senator in room
+  enter_room(number);
+  vote(number);
+  sem_post(&semaphore->senador); // Exit Critical Section
+}
 
 int main(){
   srand(time(NULL));
-  int senador   = rand() % 50;
+  int senador   = 5;
   int deputado  = rand() % 50;
   int vereador  = rand() % 50;
   int i; // counter
-  int status; // waiting status
-
-  
-  key_t key = KEY;
+  int status;
+  int pid;
+  int number;
+  key_t key = 5678;
   int shm_id;
-  shm_id = shm_create (key);
-  sems *attr;
-  attr = (sems) shmat (shm_id, NULL, 0);
-  sem_init(attr->senador, 1, 1);	   
+  Sems *semaphore;
+  
+  //create memmory area
+  shm_id = shm_create (semaphore);
+ 
+  //atach semaphore to memmory area
+  semaphore = shm_attach(shm_id);
+
+ // shm_write_process(semaphore);
+  
+  //initiates semaphores 
+  sem_init(&semaphore->senador,1,1);
 
   // Creating the 'Senadores'
   for(i=0;i<senador;i++){
+    printf("Fork %d\n",i);
     pid = fork();
-    if(pid < 0){printf("Failed to fork!\n");}
-    else if(pid==0){ // Child
-      // Starting voting
-      Parlamentar new_parlamentar;
-      new_parlamentar.type = SENADOR;
+    if(pid==0){
+      number = i;
+      break;
     }
   }
-
-  // Creating the 'Deputados'
-  for(i=0;i<deputado;i++){
-    pid = fork();
-    if(pid < 0){printf("Failed to fork!\n");}
-    else if(pid==0){ // Child
-      // Starting voting
-      Parlamentar new_parlamentar;
-      new_parlamentar.type = DEPUTADO;
-    }
+  if(pid==0){
+    voting_process(number,shm_id);
   }
-
-  // Creating the 'Vereadores'
-  for(i=0;i<vereador;i++){
-    pid = fork();
-    if(pid < 0){printf("Failed to fork!\n");}
-    else if(pid==0){ // Child
-      // Starting voting
-      Parlamentar new_parlamentar;
-      new_parlamentar.type = VEREADOR;
-    }
+  sem_destroy(&semaphore->senador);
+  for(int a = 0; a <senador;a++){
+   wait(&status);
   }
-
-  wait(&status);
   return 0;
 }
